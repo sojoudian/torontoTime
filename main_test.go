a54d1a2bc00175ad3c5509be6233e30cabeff8c8
Maziar Sojoudian <maziar@sojoudian.net>
2023-11-09 17:54:30 -0500

    add go test for getTime() func

--- main_test.go
+++ main_test.go
@@ -8,27 +8,35 @@ import (
 	"time"
 )
 
-func TestGetTime(t *testing.T) {
-	//Create a req
+func TestGetTimeAPI(t *testing.T) {
+	// Create a request to pass to our handler.
 	req, err := http.NewRequest("GET", "/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Create a ResponseRecorder to record the response.
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(getTime)
+
+	// Perform the request
 	handler.ServeHTTP(rr, req)
 
 	// Check the status code is what we expect.
 	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Handler returend worng status code: got %v want %v", status, http.StatusOK)
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
+
+	// Check the response body is what we expect.
 	var response TimeResponse
 	err = json.Unmarshal(rr.Body.Bytes(), &response)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Verify that the response contains a valid time
 	_, err = time.Parse("2006-1-2 15:4:5", response.CurrentTime)
 	if err != nil {
-		t.Errorf("Handler returend unexpected body: got %v", response.CurrentTime)
+		t.Errorf("handler returned unexpected body: got %v", response.CurrentTime)
 	}
 }
